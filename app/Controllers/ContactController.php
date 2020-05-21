30fa97b590b4e5a2128b25eff13f44cc1f202ced
zeeshan <zeeshan.shaikh@proclivistech.com>
2020-05-22 02:46:51 +0530

    mail added

--- app/Controllers/ContactController.php
+++ app/Controllers/ContactController.php
@@ -25,6 +25,41 @@ class ContactController extends BaseController
 		$request = \Config\Services::request();
 		$post_data =  $request->getRawInput();
 		//print_r($post_data);exit;
+		
+		$to = "zeeshan.sask@gmail.com, shabbiribnephiroze@gmail.com";
+		$subject = "Enquiry";
+
+		$message = "
+		<html>
+			<head>
+				<title>HTML email</title>
+			</head>
+			<body>
+				<p>This email contains HTML Tags!</p>
+				<table>
+				<tr>
+					<th>Firstname</th>
+					<th>Lastname</th>
+				</tr>
+				<tr>
+					<td>John</td>
+					<td>Doe</td>
+				</tr>
+				</table>
+			</body>
+		</html>
+		";
+
+		// Always set content-type when sending HTML email
+		$headers = "MIME-Version: 1.0" . "\r\n";
+		$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+		// More headers
+		$headers .= 'From: <diligenttax@diligenttaxservices.in>' . "\r\n";
+		$headers .= 'Cc: myboss@example.com' . "\r\n";
+
+		mail($to,$subject,$message,$headers);
+
 		$session->set('thank_msg', '1');
 		return redirect()->back();
 		//return view('pages/contact');
